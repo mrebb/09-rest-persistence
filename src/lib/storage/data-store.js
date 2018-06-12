ab06d1d10f81a23217179efe4b619ce0762687cb
Mrebb <madhu.rebbana@gmail.com>
2018-06-11 19:03:59 -0700

    storage changed to filesystem

--- src/lib/storage/data-store.js
+++ src/lib/storage/data-store.js
@@ -14,10 +14,10 @@ console.log(process.env.STORAGE);
 switch( process.env.STORAGE ) {
 case 'filesystem':
   console.log('i reached data store');
-  dataStorageModule = fileStorage;
+  dataStorageModule = memoryStorage;
   break;
 default:
-  dataStorageModule = memoryStorage;
+  dataStorageModule = fileStorage;
   break;
 }
 
